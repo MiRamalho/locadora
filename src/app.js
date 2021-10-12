5280e24ea13ccdba0813ef23801aa7a37aedc754
Michelle <mimi.ramalho@gmail.com>
2021-10-11 23:10:30 -0300

    class date: 11-10-2021

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /* ** ESSE AQUI EH O PONTO DE ENTRADA ** ELE QUE SERA O SERVIDOR DA API
-   Para importar libs ou arquivos, utiliza-se o comando require
 
-   () executa um metodo
+    Para importar libs ou arquivos, utiliza-se o comando require
+
+   () ==> executa um metodo
 
 */
 
-//carrego a lib do express (servidor da aplicacao) - importou o objeto
+//declarando uma constante (variavel) chamada express
+//carrego a lib do express (servidor da aplicacao) - importando o objeto (a biblioteca)
 const express = require('express') 
 
 //importa as rotas
@@ -24,7 +26,7 @@ app.use(express.json())
 //importa as rotas para o servidor (app.js)
 app.use('/', routes) 
 
-// crio uma constante para definir que porta o servidor rodarah 
+// crio uma constante (variavel) para definir que porta o servidor rodarah 
 const port = 3000 /* foi criada uma variavel chamada porta e adicionado o valor de 3000, mas poderia ser qualquer outro valor, menos 8080 pois eh a porta default da internet */
                   /* quando estamos trabalhando com APIs, sempre tem que ser direcionado para alguma porta. A porta vai influenciar na hora de subir o servidor */
 
@@ -34,6 +36,8 @@ CODIGO ORGANIZADO */
  //estah sendo utilizado o metodo GET, que eh uma consulta no servidor e ela serah acessada pela /
  
  /*app.get('/helloworld', (req, res) => {
+   app.get('/helloworld', function(req, res) {    eh a mesma coisa da linha de cima, escrita de forma diferente
+   
     //call back (retorno de uma funcao)
     //entao o segundo parametro do GET, eh uma funcao com dois parametros que sao:
     //o req = request (que eh a requisicao) e 
@@ -45,8 +49,9 @@ CODIGO ORGANIZADO */
 //listen = ouvir/escutar uma determinada porta que nesse caso serah a 3000
 //entao nesse caso o servidor da aplicacao vai subir no localhost atendendo a porta 3000
 app.listen(port, function() {
+//app.listen(port, () => {
  
-  //const MIMI = new Customer(1,"Michelle","22162741865","11031982")
+   //const MIMI = new Customer(1,"Michelle","22162741865","11031982")
   //console.log('Valor de customers:', MIMI)
   console.log(`Example app listening at http://localhost:${port}`)
 })
